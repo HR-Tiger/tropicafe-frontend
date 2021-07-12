63d173d4d81376297bfb8883e33f3655995cee75
Chris McClelland <clm727@gmail.com>
2021-07-12 17:06:40 -0400

    remove shopHeader from app`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './styles/sass.scss';
 import Navigation from './Navigation.jsx';
-import ShopHeader from './components/shop/ShopHeader.jsx';
 
 export default function App() {
   return (
     <>
       <Navigation />
-      <ShopHeader />
     </>
   );
 }
