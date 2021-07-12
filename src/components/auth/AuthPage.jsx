f02a8bcfe2679097c454d606c7e709008b7dc4fb
Zachary Sawyer <mountainsounds15@gmail.com>
2021-07-12 08:43:49 -0400

    seperated auth components into seperate modules

--- src/components/auth/AuthPage.jsx
+++ src/components/auth/AuthPage.jsx
@@ -1,42 +1,7 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
-function Input({type, name, id, placeholder}) {
-	return (
-			<div className="Input">
-				<label htmlFor={id}>{name}</label>
-				<input id={id} autoComplete="false" required type={type} placeholder={placeholder} />
-			</div>
-	)
-}
-
-function Login({flipCard, onSubmit}) {
-		return (
-			<div className="login">
-				<form onSubmit={onSubmit} className="ModalForm">
-					<h3>Log In <span className="flipper" onClick={flipCard}>Sign Up</span></h3>
-					<Input id="email" name="Email" type="email" placeholder="name@gmail.com" />
-					<Input id="password" name="Password" type="password" placeholder="password" />
-					<button>Log in</button>
-				</form>
-			</div>
-		);
-	}
-
-function Signup({flipCard, onSubmit}) {
-		return (
-			<div className="login">
-				<form onSubmit={onSubmit} className="ModalForm">
-					<h3>Sign Up<span className="flipper" onClick={flipCard}>Log In</span></h3>
-          <Input id="fName" name="First Name" type="text" placeholder="Ruby" />
-          <Input id="lName" name="Last Name" type="text" placeholder="Von Rails" />
-					<Input id="email" name="Email" type="email" placeholder="ruby@gmail.com" />
-					<Input id="password" name="Password" type="password" placeholder="password" />
-					<button>Log in</button>
-				</form>
-			</div>
-		);
-	}
+import Login from './Login.jsx';
+import Registration from './Registration.jsx';
 
 export default function AuthPage(props) {
   const [isLogin, setIsLogin] = useState(false);
@@ -47,13 +12,13 @@ export default function AuthPage(props) {
 
 	let flipCard = () => setIsLogin(!isLogin);
 
-	let login =  (<Login onSubmit={handleSubmit} flipCard={flipCard}/>);
-  let signup = (<Signup onSubmit={handleSubmit} flipCard={flipCard}/>);
+	let login =  (<Login onSubmit={handleSubmit} flipCard={flipCard} />);
+  let registration = (<Registration onSubmit={handleSubmit} flipCard={flipCard} />);
 
-		return(
+		return (
 			<div className="authPageBody">
 				<h1>Welcome to Tropicafé!</h1>
-					{isLogin ? login : signup}
+				{isLogin ? login : registration}
 			</div>
 		);
 }
