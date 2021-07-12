068c75bf5104a1c24ae3aa7882f13485125022b4
Zachary Sawyer <mountainsounds15@gmail.com>
2021-07-12 15:24:17 -0400

    structure

--- src/components/user/UserPage.jsx
+++ src/components/user/UserPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserHeader from './UserHeader.jsx';
 
 export default function UserPage() {
   return (
-    <>
-    </>
+    <div className="profilePageBody">
+      <h1>Welcome Zack Sawyer</h1>
+    </div>
   );
 }
