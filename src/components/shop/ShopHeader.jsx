9679a3c8bd495c35ac64beb626131cc24e8d127d
Chris McClelland <clm727@gmail.com>
2021-07-12 15:47:09 -0400

    styl shop header

--- src/components/shop/ShopHeader.jsx
+++ src/components/shop/ShopHeader.jsx
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-export default function ShopHeader({ shop }) {
+let shop = {
+  'shop_id': 1,
+  'name': 'sit',
+  'address': '2 Reinke Road',
+  'city': 'Naples',
+  'state': 'FL',
+  'zip': 112,
+  'date': '1604901282000',
+  'phone_number': '239-398-1215',
+  'website': 'https://bing.com',
+  'animal_friendly': true,
+  'photos': [
+    {
+      'url': 'http://dummyimage.com/101x100.png/cc0000/ffffff'
+    },
+    {
+      'url': 'http://dummyimage.com/235x100.png/ff4444/ffffff'
+    },
+    {
+      'url': 'http://dummyimage.com/212x100.png/cc0000/ffffff'
+    }
+  ]
+};
+
+export default function ShopHeader() {
 
 
   return (
@@ -8,12 +32,28 @@ export default function ShopHeader({ shop }) {
 
       <img
         className="img-thumbnail"
-        src={shop.image}
+        src={shop.photos[0]}
       />
 
       <div className="shop-info">
 
         <div className="shop-address">
+          {shop.address}
+          {shop.city}, {shop.state} {shop.zip}
+          {shop.phone_number}
+          {shop.website}
+        </div>
+
+        <div className="card">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{shop.website}</li>
+            <li className="list-group-item">{shop.address} {shop.city} {shop.state} </li>
+            <li className="list-group-item">{shop.website}</li>
+          </ul>
+        </div>
+
+        <div>
+
         </div>
 
       </div>
@@ -21,6 +61,7 @@ export default function ShopHeader({ shop }) {
     </div>
   );
 }
+
 
 
 // {
@@ -39,3 +80,4 @@ export default function ShopHeader({ shop }) {
 //   reviews,
 //   rating
 // }
+
