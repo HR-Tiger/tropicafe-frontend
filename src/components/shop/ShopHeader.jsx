71d46ba86c97f2590b5a80b095886c0511e1d48f
Chris McClelland <clm727@gmail.com>
2021-07-12 13:53:42 -0400

    start shopHeader

---
+++ src/components/shop/ShopHeader.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+export default function ShopHeader({ shop }) {
+
+
+  return (
+    <div className="shop-header">
+
+      <img
+        className="shop-photo"
+        src={shop.image}
+      />
+
+      <div className="shop-info">
+
+        <div className="shop-address">
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+
+// {
+//   id,
+//   name,
+//   location: {
+//     address,
+//     city,
+//     state,
+//     zip
+//   }
+//   phone_number,
+//   price
+//   website,
+//   animal_friendly
+//   reviews,
+//   rating
+// }
