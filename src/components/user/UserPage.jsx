f79989c0a20b9986434448f44596377402cc42df
Edgar <edgarcrd@gmail.com>
2021-07-14 17:03:38 -0400

    Added user link to review component

--- src/components/user/UserPage.jsx
+++ src/components/user/UserPage.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import { useParams, useLocation } from 'react-router-dom';
 import UserHeader from './UserHeader.jsx';
 import ReviewList from '../../shared-components/ReviewList.jsx';
 
@@ -7,18 +8,22 @@ export default function UserPage() {
   const [userInfo, setUserInfo] = useState({});
   const [reviews, setReviews] = useState([]);
 
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
+
   let userId = 1;
   // Get User Info
-  useEffect(() => axios.get(`http://3.239.52.75/api/users/${userId}`).then(({data}) => setUserInfo(data)), []);
+  useEffect(() => axios.get(`http://3.239.52.75/api/users/${id}`).then(({data}) => setUserInfo(data)), []);
   // Get Reviews for User
-  useEffect(() => axios.get(`http://3.239.52.75/api/reviews/users/${userId}`).then(data => setReviews(data.data)), []);
+  useEffect(() => axios.get(`http://3.239.52.75/api/reviews/users/${id}`).then(data => setReviews(data.data)), []);
 
   return (
     <div className='profilePageBody'>
       <main className='profileContainer'>
         <UserHeader userInfo={userInfo} reviews={reviews} />
         <section className='reviewList'>
-          <ReviewList id={1} type={'user'}/>
+          <ReviewList id={id} type={'user'}/>
         </section>
       </main>
 
--- src/shared-components/Review.jsx
+++ src/shared-components/Review.jsx
@@ -20,7 +20,7 @@ const Review = ( { review, type } ) => {
           setShop(data.data[0]);
         })
         .catch(err => {
-          consoel.log(err);
+          console.log(err);
         });
     }, []);
   }
@@ -49,31 +49,33 @@ const Review = ( { review, type } ) => {
           });
       })
       .catch ((err) => {
-        consoel.log(err);
+        console.log(err);
       });
   };
 
   if (type === 'shop') {
-    console.log('hi', review.shop_id);
+    // console.log('hi', review.shop_id);
     if (review.user[0].profile_photo_url === null) {
       var profile_pic = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
     } else {
       var profile_pic = review.user[0].profile_photo_url;
     }
-    var username = review.user[0].first_name;
     var pathName = `/user/${review.user_id}`;
+    console.log(pathName);
+    var username = review.user[0].first_name;
     var location = `${review.user[0].city}, ${review.user[0].state}`;
   } else {
-    console.log('hey', shop.shop_id);
-    if (shop.photos === undefined) {
+    // console.log('hey', shop.photos);
+    if (shop.photos === null || shop.photos === undefined) {
       var profile_pic = 'https://www.whitevilla.co.uk/img/missing_product.png';
     } else {
       var profile_pic = shop.photos[0].url;
-      console.log(shop.photos[0]);
     }
-    // var profile_pic = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
     var username = shop.name;
-    var pathName = `/shop/${shop.shop_id}`;
+    var pathName = {
+      pathname: `/shop/${shop.shop_id}`,
+      state: {shop: shop}
+    };
     var location = `${shop.city}, ${shop.state}`;
   }
 
