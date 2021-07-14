e2d79d175c959fdec6fc5e327253786357c678ab
Chris McClelland <clm727@gmail.com>
2021-07-14 14:08:03 -0400

    added filter options

--- src/shared-components/FilterReviews.jsx
+++ src/shared-components/FilterReviews.jsx
@@ -14,12 +14,53 @@ export default function FilterReviews({ setReviewList }) {
     'Cold Brew'
   ];
 
+  const mapper = [1, 2, 3, 4, 5];
+
   return (
     <>
       <form className="row g-3 position-sticky">
         <div className="card">
           <div className="card-body">
-
+            <h5 className="card-title">Filters</h5>
+          </div>
+          <div className="card-body">
+            <h3 className="Rating"></h3>
+            <div className="form-check">
+              {mapper.map(rating => (
+                <>
+                  <input
+                    key={rating}
+                    className="form-check-input"
+                    type="checkbox"
+                    value={rating}
+                  />
+                  <label className="form-check-label">stars</label>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="card-body">
+            <h3 className="card-title">Price</h3>
+            <div className="form-check">
+              {mapper.map(price => (
+                <>
+                  <input
+                    key={price}
+                    className="form-check-input"
+                    type="checkbox"
+                    value={price}
+                  />
+                  <label className="form-check-label">$</label>
+                </>
+              ))}
+            </div>
+          </div>
+          <div className="card-body">
+            <select className="form-select">
+              {categories.map(category => (
+                <option key={category} value={category}>{category}</option>
+              ))}
+            </select>
           </div>
         </div>
       </form>
