1923fc4894539ed5617f0e571d52d74d03e34185
Chris McClelland <clm727@gmail.com>
2021-07-14 13:49:24 -0400

    filter form

--- src/shared-components/FilterReviews.jsx
+++ src/shared-components/FilterReviews.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
-export default function FilterReviews() {
+export default function FilterReviews({ setReviewList }) {
+
+  const categories = [
+    'Drip Brew',
+    'Latte',
+    'Cappucino',
+    'Americano',
+    'Espresso',
+    'Mocha',
+    'Tea',
+    'Iced Coffee',
+    'Cold Brew'
+  ];
 
   return (
     <>
+      <form className="row g-3 position-sticky">
+        <div className="card">
+          <div className="card-body">
 
+          </div>
+        </div>
+      </form>
     </>
   );
 }
