df773c8c4ba6c45de57635a61345b05cddd6dee3
Chris McClelland <clm727@gmail.com>
2021-07-15 12:22:24 -0400

    started endpoints file

--- src/lib/endpoints.js
+++ src/lib/endpoints.js
@@ -1,3 +1,10 @@
 export const endpoints = {
-
+  postReview: '/api/reviews/:shopId',
+  postShop: '/api/shops',
+  getShopReviews: (id) => (`/api/shops/${id}/reviews`);
 }
+
+
+import endpoints from './thisfile'
+import { url }
+axios.post(url + getShopReviews(shopId))
