d840ddc2192870c2b32d6b99d05679e53214a8cf
Chris McClelland <clm727@gmail.com>
2021-07-15 16:47:27 -0400

    remove test photos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
-const multer = require('multer');
+// const multer = require('multer');
 
-const upload = multer({ dest: 'image_storage/' });
+// const upload = multer({ dest: 'image_storage/' });
 
 const app = express();
 const port = 3001;
@@ -10,10 +10,10 @@ const html = path.join(__dirname, 'dist', 'index.html');
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.post('/post', upload.array('photos'), (req, res) => {
-  console.log(req.body);
-  console.log('FILES: ', req.files);
-});
+// app.post('/post', upload.array('photos'), (req, res) => {
+//   console.log(req.body);
+//   console.log('FILES: ', req.files);
+// });
 
 app.get('/*', (req, res) => res.sendFile(html));
 
