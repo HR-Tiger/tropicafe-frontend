ea6b2ae0473c6979287c482641ada820d3a53e0f
Alec Stavros <alecwstavros@gmail.com>
2021-07-16 10:55:32 -0500

    minor css updates

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -56,13 +56,13 @@ export default function Home() {
                 <>
                   <h2 className='recent-title'>Results for {searchedShops[0].city}:</h2>
                   <ShopList ShopList={searchedShops.slice(0, 9)} />
-                  <button onClick={() => setSeeMoreSearched(false)}>See Less</button>
+                  <button className='loginBtn' onClick={() => setSeeMoreSearched(false)}>See Less</button>
                 </>
               ) : (
                 <>
                   <h2 className='recent-title'>Results for {searchedShops[0].city}:</h2>
                   <ShopList ShopList={searchedShops.slice(0, 3)} />
-                  <button onClick={() => setSeeMoreSearched(true)}>See More Searched Shops</button>
+                  <button className='loginBtn' onClick={() => setSeeMoreSearched(true)}>See More Searched Shops</button>
                 </>)}
             </div>
           </>
@@ -73,13 +73,13 @@ export default function Home() {
                 {seeMorePopular ? (<>
                   <h2 className='popular-title'>Popular Coffee Shops</h2>
                   <ShopList ShopList={allPopularShops} />
-                  <button onClick={() => setSeeMorePopular(false)}>See Less</button>
+                  <button className='loginBtn' onClick={() => setSeeMorePopular(false)}>See Less</button>
                 </>
                 ) : (
                   <>
                     <h2 className='popular-title'>Popular Coffee Shops</h2>
                     <ShopList ShopList={popularShops} />
-                    <button onClick={() => setSeeMorePopular(true)}>See More Popular Shops</button>
+                    <button className='loginBtn' onClick={() => setSeeMorePopular(true)}>See More Popular Shops</button>
 
                   </>)}
               </div>
@@ -89,13 +89,13 @@ export default function Home() {
                   <>
                     <h2 className='recent-title'>Recently Added Coffee Shops</h2>
                     <ShopList ShopList={allRecentShops} />
-                    <button onClick={() => setSeeMoreRecent(false)}>See Less</button>
+                    <button className='loginBtn' onClick={() => setSeeMoreRecent(false)}>See Less</button>
                   </>
                 ) : (
                   <>
                     <h2 className='recent-title'>Recently Added Coffee Shops</h2>
                     <ShopList ShopList={recentShops} />
-                    <button onClick={() => setSeeMoreRecent(true)}>See More Recent Shops</button>
+                    <button className='loginBtn' onClick={() => setSeeMoreRecent(true)}>See More Recent Shops</button>
                   </>)}
               </div>
             </>)}
